b29cd92f03d33317ef4472da6f2c0686105214f4
Frederic Tingaud <ftingaud@afro-rangers.com>
2015-08-03 15:26:41 +0200

    Document settings.

--- Settings.h
+++ Settings.h
@@ -8,11 +8,40 @@ class QTextDocument;
 class Settings
 {
 public:
-  virtual FSMReader& getReader();
+  /**
+   * @return The writer used to save the FSM chart.
+   */
   virtual FSMWriter& getWriter();
+
+  /**
+   * @return The reader used to load files written by the FSMWriter.
+   */
+  virtual FSMReader& getReader();
+
+  /**
+   * We use a QTextEdit to edit code. It can display code with syntax highlighting.
+   */
   virtual void initializeCodeHighlighter(QTextDocument* document);
+
+  /**
+   * @return the file extension used for chart files. @Default is "fsm".
+   */
   virtual QString getExportExtension() const;
+
+  /**
+   * @return the organization name used to store data in registry between sessions.
+   */
   virtual QString getOrganizationName() const;
+
+  /**
+   * @return the application name used to store data in registry between sessions.
+   */
   virtual QString getApplicationName() const;
+
+  /**
+   * Validates whether the new state name is correctly formed for export.
+   * @return the error message if any. Empty string for no error.
+   * @Default forbids whitespaces.
+   */
   virtual QString validateStateName(const QString& name);
 };
