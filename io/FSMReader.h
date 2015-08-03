41bffd1e2c25c2dab3c9d8697d714b3b6cbfa6e6
Frederic Tingaud <ftingaud@afro-rangers.com>
2015-08-03 15:31:24 +0200

    Document reader and writer.

--- io/FSMReader.h
+++ io/FSMReader.h
@@ -9,7 +9,13 @@ class QTextStream;
 class FSMReader
 {
 public:
+  /**
+   * Read from an inputStream and outputs a graph.
+   * @return The loaded graph.
+   */
   virtual Graph read(QTextStream& inStream);
+
+private:
   GraphState* readState(const QString& name, QTextStream& inStream);
   GraphTransition* readTransition(const QString& origin, const QString& destination, QTextStream& inStream);
 };
--- io/FSMWriter.h
+++ io/FSMWriter.h
@@ -6,5 +6,8 @@ class QTextStream;
 class FSMWriter
 {
 public:
+  /**
+   * Writes into given output stream the passed graph.
+   */
   void write(Graph& scene, QTextStream& out);
 };
